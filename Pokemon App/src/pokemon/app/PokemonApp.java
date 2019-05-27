c218de99194178477f75d425c1876648e91e5c17
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-27 11:59:43 -0500

    Creación inicial de la aplicación y carpetas entidades e interfaces

---
+++ Pokemon App/src/pokemon/app/PokemonApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon.app;
+
+/**
+ *
+ * @author alumno
+ */
+public class PokemonApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
